701e8a2ca0982afa3dd3051c29b403efde35c703
Sergey Popkov <serega1986-16@mail.ru>
2021-12-08 23:09:35 +0300

    Обновил миграцию под Postgresql

--- db/migrations/init-database.sql
+++ db/migrations/init-database.sql
@@ -1,75 +1,52 @@
-IF NOT EXISTS(SELECT * FROM master.dbo.sysdatabases WHERE name = 'Impuls')
-BEGIN
-    CREATE DATABASE Impuls
-END
+CREATE DATABASE Impuls
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'account' AND type = 'U')
-BEGIN
-    CREATE TABLE account (
-        [id_account] BIGSERIAL IDENTITY(1,1) PRIMARY KEY NOT NULL,
-        [email] VARCHAR(100) UNIQUE NOT NULL,
-        [pass] VARCHAR(100) NOT NULL,
-        [name] VARCHAR(MAX) NOT NULL
-    );
-END
+CREATE TABLE IF NOT EXISTS account (
+    id_account BIGSERIAL PRIMARY KEY NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    pass VARCHAR(100) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'role' AND type = 'U')
-BEGIN
-    CREATE TABLE role (
-        [id_role] TINYINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-        [name] VARCHAR(30) NOT NULL,
-    );
-END
+CREATE TABLE IF NOT EXISTS role (
+    id_role SMALLINT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'account_role' AND type = 'U')
-BEGIN
-    CREATE TABLE account_role (
-        [id_account] BIGSERIAL PRIMARY KEY NOT NULL,
-        [id_role] TINYINT PRIMARY KEY NOT NULL,
-        CONSTRAINT FK_account_role_account FOREIGN KEY (id_account)
-        REFERENCES account(id_account)
-    );
-END
+CREATE TABLE IF NOT EXISTS account_role (
+    id_account BIGSERIAL NOT NULL,
+    id_role SMALLINT NOT NULL,
+    PRIMARY KEY (id_account, id_role),
+    CONSTRAINT FK_account_role_account FOREIGN KEY (id_account)
+    REFERENCES account(id_account)
+);
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'game' AND type = 'U')
-BEGIN
-    CREATE TABLE game (
-        [id_game] TINYINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-        [name] VARCHAR(100) NOT NULL,
-        [description] VARCHAR(MAX)
-    );
-END
+CREATE TABLE IF NOT EXISTS game (
+    id_game SMALLINT PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT
+);
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'game_result' AND type = 'U')
-BEGIN
-    CREATE TABLE game_result (
-        [id_game_result] BIGSERIAL IDENTITY(1,1) PRIMARY KEY NOT NULL,
-        [id_account] BIGSERIAL NOT NULL,
-        [id_game] TINYINT  NOT NULL,
-        [quantity_tasks] INT NOT NULL,
-        [quantity_true_answers] INT NOT NULL,
-        CONSTRAINT FK_game_result_game FOREIGN KEY (id_game)
-        REFERENCES game(id_game)
-        CONSTRAINT FK_game_result_account FOREIGN KEY (id_account)
-        REFERENCES account(id_account)
-    );
-END
+CREATE TABLE IF NOT EXISTS game_result (
+    id_game_result BIGSERIAL PRIMARY KEY NOT NULL,
+    id_account BIGSERIAL NOT NULL,
+    id_game SMALLINT  NOT NULL,
+    quantity_tasks INT NOT NULL,
+    quantity_true_answers INT NOT NULL,
+    CONSTRAINT FK_game_result_game FOREIGN KEY (id_game)
+    REFERENCES game(id_game),
+    CONSTRAINT FK_game_result_account FOREIGN KEY (id_account)
+    REFERENCES account(id_account)
+);
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'complexity' AND type = 'U')
-BEGIN
-    CREATE TABLE complexity (
-        [id_complexity] TINYINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-        [complexity_kind] TINYINT NOT NULL
-    );
-END
+CREATE TABLE IF NOT EXISTS complexity (
+    id_complexity SMALLINT PRIMARY KEY NOT NULL,
+    complexity_kind SMALLINT NOT NULL
+);
 
-IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'game_complexity' AND type = 'U')
-BEGIN
-    CREATE TABLE game_complexity (
-        [id_game_complexity] BIGSERIAL IDENTITY(1,1) PRIMARY KEY NOT NULL,
-        [id_game] TINYINT  NOT NULL,
-        [id_complexity] TINYINT NOT NULL,
-        CONSTRAINT FK_game_complexity_game FOREIGN KEY (id_game)
-        REFERENCES game(id_game)
-    );
-END
+CREATE TABLE IF NOT EXISTS game_complexity (
+    id_game_complexity BIGSERIAL PRIMARY KEY NOT NULL,
+    id_game SMALLINT NOT NULL,
+    id_complexity SMALLINT NOT NULL,
+    CONSTRAINT FK_game_complexity_game FOREIGN KEY (id_game)
+    REFERENCES game(id_game)
+);
